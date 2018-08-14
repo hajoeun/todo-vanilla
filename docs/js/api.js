7f9d85c6b1b3882c6166e21a94a0aace842944ee
Joeun <imjoeunha@gmail.com>
2018-08-14 23:39:37 +0900

    update: api, app

--- docs/js/api.js
+++ docs/js/api.js
@@ -2,14 +2,30 @@
   const web_sql = openDatabase('todoDB', '1', 'todo database', 5 * 1024 * 1024);
   web_sql.transaction(tx => tx.executeSql('CREATE TABLE IF NOT EXISTS todos(id INTEGER, title TEXT, completed INTEGER)', []));
 
+  const make_query = obj =>
+    Object.keys(obj).reduce((q, k) => `${q}${k}=${obj[k]} `, '');
+    
   const apis = {
+    '/todo/all': data => new Promise(function(rs) {
+      web_sql.transaction(tx => {
+        return tx.executeSql(`SELECT * FROM todos`, [], (t, { rows }) => { rs(rows) })
+      })
+    }),
     '/todo/insert': data => new Promise(function (rs) {
       web_sql.transaction(tx => {
         let id = Date.now();
         return tx.executeSql('INSERT INTO todos (id, title, completed) VALUES (?,?,?)',
-          [id, data.text, data.isCompleted ? 1 : 0],
-          () => rs({ id: id, text: data.text, isCompleted: data.isCompleted }));
+          [id, data.title, data.completed ? 1 : 0],
+          () => rs({ id: id, title: data.title, completed: data.completed }));
       })
+    }),
+    '/todo/delete': data => new Promise(function(rs) {
+        web_sql.transaction(tx =>
+          tx.executeSql('DELETE FROM todos WHERE id=?', [data.id], () => { rs({ id: data.id }) }))
+    }),
+    '/todo/update': ({ set, where }) => new Promise(function(rs) {
+      web_sql.transaction(tx =>
+        tx.executeSql(`UPDATE todos SET ${make_query(set)} WHERE ${make_query(where)}`, [], () => { rs(set) }))
     })
   };
 
--- docs/js/app.js
+++ docs/js/app.js
@@ -5,26 +5,52 @@ $.el = html => {
   let tmp = document.createElement('div');
   tmp.innerHTML = html;
   return tmp.children[0];
-}
+};
 $.appendChild = (target, element) => {
-  if (typeof target === 'string') target = $1(target);
-  if (typeof element === 'string') element = $.el(element);
+  if (typeof target == 'string') target = $1(target);
+  if (typeof element == 'string') element = $.el(element);
   target.appendChild(element);
-  return target;
-}
+  return element;
+};
+$.attr = (target, attr, value) => {
+  return value === undefined ? 
+    target.getAttribute(attr) : target.setAttribute(attr, value);
+};
 
-!function() {
+!async function() {
+
+  const li_html = data => `
+    <li class="${data.completed ? 'completed' : ''}" data-id="${data.id}">
+      <input type="checkbox" class="toggle" ${data.completed ? 'checked' : ''}>
+      <label>${data.title}</label>
+      <button class="delete"></button>
+    </li>`;
+
+  const delete_event = async ({ currentTarget }) => {
+    await fetch('/todo/delete', { id: $.attr(currentTarget.parentNode, 'data-id') });
+    currentTarget.parentNode.remove();
+  }
+
+  const check_event = async ({ currentTarget }) => {
+    let set = { completed: currentTarget.checked ? 1 : 0 };
+    let where = { id: $.attr(currentTarget.parentNode, 'data-id') };
+    let res = await fetch('/todo/update', { set, where });
+    currentTarget.parentNode.classList = res.completed ? 'completed' : '';
+  }
+
+  for (let data of await fetch('/todo/all', {})) {
+    let li_element = $.appendChild('ul.todo-list', li_html(data));
+    li_element.querySelector('.delete').onclick = delete_event;
+    li_element.querySelector('.toggle').onclick = check_event;
+  }
+
   $.on('input.new-todo', 'keydown', async ({ currentTarget, keyCode }) => {
     if (keyCode !== 13) return;
-    let data = await fetch('/todo/insert', { text: currentTarget.value, isCompleted: false });
-    let new_li = $.el(`
-      <li class="${data.isCompleted ? 'completed' : ''}" data-id="${data.id}">
-        <input type="checkbox" class="toggle">
-        <label>${data.text}</label>
-        <button class="delete"></button>
-      </li>`);
-
-    $.appendChild('ul.todo-list', new_li);
+    let data = await fetch('/todo/insert', { title: currentTarget.value, completed: false });
+    let li_element = $.appendChild('ul.todo-list', li_html(data));
+    li_element.querySelector('.delete').onclick = delete_event;
+    li_element.querySelector('.toggle').onclick = check_event;
+    currentTarget.value = '';
   });
 
 }()
